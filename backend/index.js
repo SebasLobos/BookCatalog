98c0e238c5f2e477bccd273fc192fb9b2ee662ef
Sebastian <lobos87@gmail.com>
2020-11-25 15:37:44 -0300

    created schema, connected MDBdatabase and created .env file

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
+
+if (process.env.NODE_ENV !== 'production'){
+    require ('dotenv').config();
+}
+
 const express = require('express');
 const morgan = require ('morgan');
 const multer = require ('multer');
 const path = require('path')
+
 //Initializacions
 const app = express();
+require('./database');
 
 //Settings
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 
 //Middlewares
 app.use(morgan('dev'));
@@ -19,6 +26,12 @@ const storage = multer.diskStorage({
 app.use(multer({storage}).single('image'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+//Routes
+app.use('/api/books', require('./routes/books'));
+
+//Static files
+app.use(express.static(path.join(__dirname,'public')));
 
 //Start the server
 app.listen( app.get('port'), ()=>{
