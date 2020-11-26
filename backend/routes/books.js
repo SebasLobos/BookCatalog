3746e2a50e8822d459d1309a8704b4b5067e6ad7
Sebastian <lobos87@gmail.com>
2020-11-26 16:08:12 -0300

    Create the get, post and delete routers. Partially configured Webpack. Create the Frontend Structure

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,8 +1,24 @@
 const { Router } = require('express');
+const Book = require('../models/Book');
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({text: 'Hello World'});
+const book = require('../models/Book')
+
+router.get('/', async (req, res) => {
+    const books = await book.find();
+    res.json(books)
+});
+
+router.post('/', async (req, res)=> {
+ const {title, author, isbn} = req.body;
+ newBook = new book({title, author, isbn});
+ await newBook.save();
+ res.json({message : "Book Saved"});
+});
+
+router.delete('/:id', async (req, res) => {
+    await book.findByIdAndDelete(req.params.id);
+    res.json({message: 'Book Deleted'});
 });
 
 module.exports = router;
